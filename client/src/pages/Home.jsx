8a4a7c72004ba820ebbcd22c7239deea2a4e5d93
Arshit790 <arshitdixit5@gmail.com>
2024-04-18 11:22:03 +0530

    "third commit"

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
-
+import { useAuth } from "../context/Auth";
 const Home = () => {
+  const [auth,setAuth] = useAuth()
   return (
     <Layout title={'Home-Ecommerce App'}>
       <h1>HomePage</h1>
-      <h1>Hello Welcome to my website</h1>
+     <pre>{JSON.stringify(auth,null,4)}</pre>
     </Layout>
   );
 };
