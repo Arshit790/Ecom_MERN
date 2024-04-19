300b3075a22f7061f113480dfb3d6f1cd6fab5b6
Arshit790 <arshitdixit5@gmail.com>
2024-04-20 01:09:10 +0530

    "updates"

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-import express from 'express'
-import colors from 'colors'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import { connect } from 'mongoose'
-import connectDB from './config/db.js'
-import authRoutes from './routes/authRoute.js'
-import cors from 'cors'
-
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { connect } from "mongoose";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
 //configure env
-dotenv.config()
+dotenv.config();
 
 //db config
 connectDB();
 
 //rest obj
-const app = express()
+const app = express();
 
 //middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-//routes 
-app.use('/api/v1/auth',authRoutes)
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to ecommerce app",
+  });
+});
 
-
-app.get('/', (req,res)=>{
-    res.send({
-        message : "Welcome to ecommerce app"
-    })
-})
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(` Server is Running Fine!! ${port}`.bgCyan.white)
-})
+  console.log(` Server is Running Fine!! ${port}`.bgCyan.white);
+});
